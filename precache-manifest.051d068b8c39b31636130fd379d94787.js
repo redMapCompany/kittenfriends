cb02052c381916f58f41306ebdcae6cc776973e4
redMapCompany <okyskaa@gmail.com>
2019-02-08 16:53:26 -0800

    Updates

---
+++ precache-manifest.051d068b8c39b31636130fd379d94787.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = [
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/kittenfriends/static/media/SEGA.12a3db8b.woff"
+  },
+  {
+    "revision": "8107ed709953cebe34fb",
+    "url": "/kittenfriends/static/js/runtime~main.8107ed70.js"
+  },
+  {
+    "revision": "ec8808becda92e9fa98a",
+    "url": "/kittenfriends/static/js/main.ec8808be.chunk.js"
+  },
+  {
+    "revision": "038cb75f6f38a30eb8f7",
+    "url": "/kittenfriends/static/js/1.038cb75f.chunk.js"
+  },
+  {
+    "revision": "ec8808becda92e9fa98a",
+    "url": "/kittenfriends/static/css/main.6c26027b.chunk.css"
+  },
+  {
+    "revision": "038cb75f6f38a30eb8f7",
+    "url": "/kittenfriends/static/css/1.023cd8e4.chunk.css"
+  },
+  {
+    "revision": "b1f39669b7f643c8e784986195fcf978",
+    "url": "/kittenfriends/index.html"
+  }
+];
